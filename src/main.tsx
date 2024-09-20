77a9ea6e1c509626d29093bd5bb110e8db712f52
terasum <terasum@163.com>
2024-09-20 11:16:49 +0800

    feat(src-rust): add command+q behavior

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,13 @@ import { Toaster } from "@/components/ui/toaster";
 import { Provider } from "react-redux";
 import store from "@/store";
 
+window.addEventListener("load", (_event) => {
+  setTimeout(async () => {
+    const appWindow = (await import("@tauri-apps/api/window")).appWindow;
+    appWindow.show();
+  }, 100);
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>

1fad25b1826982951f6f48f621a12ecab40bcee1
terasum <terasum@163.com>
2024-09-24 11:18:03 +0800

    chore(src-tauri): change src-tauri as submodule

---
+++ scripts/tauri-wrapper.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+FILEPATH=$(readlink -f $0)
+ROOTDIR=$(readlink -f "$(dirname "$FILEPATH")/../")
+echo $ROOTDIR
+echo args: $@
+if [ -e src-tauri/Cargo.toml ]; then
+    sed "s|#__ROOTDIR__#|${ROOTDIR}|g" src-tauri/tauri.conf.template.json > src-tauri/tauri.conf.json
+    npx tauri $@
+else
+    echo "src-tauri/Cargo.toml not found, skipping tauri commands"
+fi

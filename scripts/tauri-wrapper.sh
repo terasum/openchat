2fd3dcab5f33548cc1a7b3a6e9305835f0f31aae
terasum <terasum@163.com>
2024-09-24 12:18:29 +0800

    fix(.github/action): fix wrapper script

--- scripts/tauri-wrapper.sh
+++ scripts/tauri-wrapper.sh
@@ -2,11 +2,11 @@
 set -evx
 FILEPATH=$(readlink -f $0)
 ROOTDIR=$(readlink -f "$(dirname "$FILEPATH")/../")
-echo $ROOTDIR
+echo ${ROOTDIR}
 echo args: $@
-if [ -e src-tauri/Cargo.toml ]; then
-    sed "s|#__ROOTDIR__#|${ROOTDIR}|g" src-tauri/tauri.conf.template.json > src-tauri/tauri.conf.json
+if [ -e "${ROOTDIR}/src-tauri/Cargo.toml" ]; then
+    sed "s|#__ROOTDIR__#|${ROOTDIR}|g" ${ROOTDIR}/src-tauri/tauri.conf.template.json > ${ROOTDIR}/src-tauri/tauri.conf.json
     npx tauri $@
 else
-    echo "src-tauri/Cargo.toml not found, skipping tauri commands"
+    echo "${ROOTDIR}src-tauri/Cargo.toml not found, skipping tauri commands"
 fi
